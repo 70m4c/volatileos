bb869ebd80300699bf79ecb9bfa87738ac617884
Томас <70m4c@70m4c.su>
2022-05-09 09:08:28 -0500

    Added required exports to XDG variables

--- archiso/airootfs/etc/profile.d/0-XDG.sh
+++ archiso/airootfs/etc/profile.d/0-XDG.sh
@@ -1,4 +1,4 @@
-XDG_CONFIG_HOME=$HOME/.config
-XDG_DATA_HOME=$HOME/.local/share
-XDG_STATE_HOME=$HOME/.local/state
-XDG_CACHE_HOME=$HOME/.cache
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_STATE_HOME=$HOME/.local/state
+export XDG_CACHE_HOME=$HOME/.cache
